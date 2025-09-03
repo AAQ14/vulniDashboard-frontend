5130c789b205625aee20e93f92384ae5a1e9b27d
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-03 19:31:43 +0300

    intial navbar created

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const NavBar = () => {
+  return (
+    <>
+        <Link to="/">HOME</Link>
+        <Link to="/vulnerabilities">Vulnerabilities</Link>
+        <Link to="/applications">Applications</Link>
+        <Link to="/profile">profile</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+    </>
+  )
+}
+
+export default NavBar
