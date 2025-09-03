f7ced6af1847508cc1930d44c811a647d227d0b9
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-03 19:37:35 +0300

    initial routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
+import { jwtDecode } from 'jwt-decode'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router'
+import { useState } from 'react'
+
 import SignupForm from './components/SignupForm/SignupForm'
 import LoginForm from './components/LoginForm/LoginForm'
+import Home from './components/Home/Home'
+import Vulnerabilities from './components/Vulnerabilities/Vulnerabilities'
+import Applications from './components/Applications/Applications'
+import NavBar from './components/NavBar/NavBar'
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  function handleLogin(newToken){
+    setToken(newToken)
+  }
+
+  function handleLogout(){
+    setToken(null)
+    localStorage.removeItem('token')
+  }
+
+  if(token){
+    const decodedToken = jwtDecode(token)
+    console.log(decodedToken)
+  }
+
+
   return (
     <>
-      <SignupForm />
-      <LoginForm />
+      
+      <BrowserRouter>
+      < NavBar/>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/login" element={<LoginForm />}/>
+            <Route path="/signup" element={<SignupForm />}/>
+            <Route path="/vulnerabilities" element={<Vulnerabilities />}/>
+            <Route path="/applications" element={<Applications />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
