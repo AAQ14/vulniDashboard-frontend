0b276f5d5ada6a668ebb5d043461db7db2f31344
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-05 15:56:14 +0300

    show all apps

--- src/components/Applications/Applications.jsx
+++ src/components/Applications/Applications.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { appIndex,appDetails } from '../../services/appService'
+import { useState, useEffect } from 'react'
 
 const Applications = () => {
+  const [apps, setApps] = useState([])
+  const [detailsView, setDetailsView] = useState(false)
+  // const [selectedApp, setSelectedApp] = ({})
+
+  async function getAllApps() {
+    try {
+      const allApps = await appIndex()
+      setApps(allApps)
+      
+    } catch (err) {
+      return err
+    }
+  }
+
+  function handleDetailsView()
+  {
+    setDetailsView(!detailsView)
+  }
+
+  useEffect(()=>{getAllApps()},[])
+
   return (
-    <div>Applications</div>
+    <>
+      <h1>Applications</h1>
+      {apps.map((app, index) => (
+        <div key={index}>
+          <p>name: {app.appName}</p>
+          <p>type: {app.type}</p>
+          <button onClick={handleDetailsView}>details</button>
+          <hr />
+        </div>
+      ))}
+    </>
   )
 }
 
