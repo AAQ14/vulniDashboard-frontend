2e85392f75cfc709b2c4b459d900af7d81251a51
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-08 14:11:11 +0300

    get the system details function done

--- src/services/systemService.js
+++ src/services/systemService.js
@@ -2,24 +2,27 @@ import axios from "axios"
 
 const baseURI = import.meta.baseURI
 
-const create = async() =>{
+const create = async(userId) =>{
     try {
         const url = `${baseURI}/systems/add`
-        const res = await axios.post(url)
+        const res = await axios.post(url, userId)
         return res
     } catch (err) {
         return err
     }
 }
 
-const details = async()=>{
+const details = async(userId)=>{
     try {
-        
+        const url = `${baseURI}/systems/details`
+        const res = await axios.get(url, userId)
+        return res.data
     } catch (err) {
         return err
     }
 }
 
 export {
-    create
+    create,
+    details
 }
