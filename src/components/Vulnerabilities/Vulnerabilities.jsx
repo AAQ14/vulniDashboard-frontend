b2ad7541be736d7ff9a25b3db74b6069a4d44dc7
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-09-08 09:46:43 +0300

    show message if user delete, update, add vuln

--- src/components/Vulnerabilities/Vulnerabilities.jsx
+++ src/components/Vulnerabilities/Vulnerabilities.jsx
@@ -8,6 +8,10 @@ const Vulnerabilities = () => {
       const [vulnerabilities, setVulnerabilities] = useState([])
       const [isFormOpen, setIsFormOpen] = useState(false)
       const [selected, setSelected] = useState(null)
+      const [message, setMessage] = useState(null)
+      const [status, setStatus] = useState(null)
+
+      console.log("this is selected" , selected)
 
       async function getAllVulns() {
          try {
@@ -19,6 +23,25 @@ const Vulnerabilities = () => {
          }
       }
 
+      console.log(status)
+
+      function displayMessage(){
+        if(status == 'updated'){
+            setMessage("vulnerability updated successfully")
+            setTimeout(()=>{setMessage(null)}, 3000)
+        }
+         if(status == 'added'){
+            setMessage("vulnerability created successfully")
+            setTimeout(()=>{setMessage(null)}, 3000)
+        }
+        if(status== 'deleted'){
+            setMessage("vulnerability deleted successfully")
+            setTimeout(()=>{setMessage(null)}, 3000)
+        }
+      }
+
+
+       console.log("this is message",message)
       // console.log("these are vulnerss",vulnerabilities) Q
 
       function handleFormView(){
@@ -30,13 +53,16 @@ const Vulnerabilities = () => {
       }
 
       useEffect(()=>{
-        getAllVulns()
-      }, [])
+        getAllVulns(),
+        displayMessage()
+      }, [status])
 
   return (
     <>
       <br/>
-      {isFormOpen ? <VulnerabilityForm handleFormView={handleFormView} getAllVulns={getAllVulns} selected={selected}/> : vulnerabilities.length ? <><h1>All Vulnerabilities</h1>
+      {isFormOpen ? <VulnerabilityForm handleFormView={handleFormView} getAllVulns={getAllVulns} selected={selected} setStatus={setStatus}/> : vulnerabilities.length ? 
+      <><h1>All Vulnerabilities</h1>
+      <div>{message}</div>
       <button onClick={()=>{handleSelected(null); handleFormView();}}>{isFormOpen ? 'Back' : 'Add vulnerability'}</button>
       {vulnerabilities?.map((vuln, index) => (
           <div key={index}>
@@ -49,7 +75,7 @@ const Vulnerabilities = () => {
             <p>Discovered at: {dayjs(vuln.discoveredAt).format('YYYY/MM/DD HH:mm')}</p>
             <p>resolved at: {vuln.resolvedAt==null? "not solved yet" : dayjs(vuln.resolvedAt).format('YYYY/MM/DD HH:mm')}</p>
             
-            <button onClick={async()=>{await deleteVuln(vuln._id); getAllVulns()}}>Delete</button>
+            <button onClick={async()=>{await deleteVuln(vuln._id); getAllVulns(); setStatus('deleted')}}>Delete</button>
 
             <button  onClick={()=>{handleSelected(vuln); handleFormView();}}>Update</button>
             <hr/>
